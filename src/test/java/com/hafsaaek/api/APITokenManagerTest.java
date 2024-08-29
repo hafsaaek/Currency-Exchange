0991791ac9912d38b3755bbb11f91bd6ec02ccd8
Hafsa Kahiye <xhafsaa20@gmail.com>
2024-08-29 16:05:45 +0100

    chore: edit  API Token manager test

--- src/test/java/com/hafsaaek/api/APITokenManagerTest.java
+++ src/test/java/com/hafsaaek/api/APITokenManagerTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
-import com.github.stefanbirkner.systemlambda.SystemLambda; // Correct import statement
+import com.github.stefanbirkner.systemlambda.SystemLambda; 
 
 public class APITokenManagerTest {
 
@@ -19,36 +19,30 @@ public class APITokenManagerTest {
         System.clearProperty("api.currency.token"); // clear the system property
     }
 
+    // Java does not allow doesn't allow setting environment variables directly through a specific class or exisiting method - we will need to create a mock environment of this using a library such as System Lambda or System Rules. We will test using both
+
     @Rule
     public final EnvironmentVariables envVariables = new EnvironmentVariables();
-
     @Test
-    public void testEnvVariableValiditySR() { // Using system L
+    public void testEnvVariableValiditySR() { // Using System Rules to test to rerieve api token from env variable
 
         // ensure system propery is cleared before retreiving env variable
         System.clearProperty("api.currency.token");
 
-        // Java does not allow doesn't allow setting environment variables directly
-        // through a specific class or exisiting method - we will need to create a mock
-        // environment of this using a library such as System Lambda or System Rules
-
-        // 1. create an instance of EnvironmentVairables and import it
-        // 2. set the env variable using System rukles
+        
         envVariables.set("API_CURRENCY_TOKEN", "test-api-token-from-env-variable");
 
         String apiKey = ApiTokenManager.getApiKey();
 
         assertEquals("test-api-token-from-env-variable", apiKey);
-
     }
 
     @Test
-    public void testEnvVariableValiditySL() throws Exception { // Using SystemLambda
+    public void testEnvVariableValiditySL() throws Exception { // Using SystemLambda to test to rerieve api token from env variable
         // Use SystemLambda to mock the environment variable
         SystemLambda.withEnvironmentVariable("API_CURRENCY_TOKEN", "test-api-token-from-env-variable")
                 .execute(() -> {
-                    System.clearProperty("api.currency.token"); // Ensure system property is cleared before retrieving
-                                                                // env variable
+                    System.clearProperty("api.currency.token"); // Ensure system property is cleared before retrieving env variable
                     assertEquals("test-api-token-from-env-variable", ApiTokenManager.getApiKey());
                 });
     }
